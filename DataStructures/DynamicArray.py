04c0db0ddce617c1cb589f1164c9a91889d7c0c4
acao2002 <xuanan12072002@gmail.com>
2021-02-01 14:07:08 -0500

    1 test case done for dynamic array

--- DataStructures/DynamicArray.py
+++ DataStructures/DynamicArray.py
@@ -1,26 +1,23 @@
 import math 
-queries = [1, 0, 5,
-1, 1, 7,
-1, 0, 3,
-2, 1, 0,
-2, 1, 1]
+queries = [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]]
 def dynamicArray(n, queries):
-    arr =[n][0]
+    arr = []
+    for m in range(n):
+        arr.append([])
     k = len(queries)
     la = 0 
     result = []
-    for i in range(0,k,3):
-        if queries[i] == 1:
-            arr[((i+1)^la)%n].append(i+2)
-            print('la 1')
-        elif i == 2:
-            la = arr[((i+1)^la)%n][i+2]
+    for i in queries:
+        if i[0] == 1:
+            arr[((i[1])^la)%n].append(i[2])
+        else:
+            la =  arr[((i[1])^la)%n][i[2]]
             result.append(la)
             print(la)
     
     return result
 
-dynamicArray(2,queries)
+print(dynamicArray(2,queries))
 
 
 
