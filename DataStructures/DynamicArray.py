d105a0bdf579eef74b33f93570335c5783242ea2
acao2002 <xuanan12072002@gmail.com>
2021-02-02 02:45:37 -0500

    finished DynamicArray

--- DataStructures/DynamicArray.py
+++ DataStructures/DynamicArray.py
@@ -1,23 +1,36 @@
 import math 
-queries = [[1, 0, 5], [1, 1, 7], [1, 0, 3], [2, 1, 0], [2, 1, 1]]
 def dynamicArray(n, queries):
     arr = []
     for m in range(n):
         arr.append([])
+    print(arr)
     k = len(queries)
     la = 0 
     result = []
     for i in queries:
         if i[0] == 1:
+            print(((i[1])^la)%n)
             arr[((i[1])^la)%n].append(i[2])
         else:
-            la =  arr[((i[1])^la)%n][i[2]]
+            print(((i[1])^la)%n)
+            la =  arr[((i[1])^la)%n][i[2]% len(arr[((i[1])^la)%n])]
             result.append(la)
             print(la)
     
     return result
 
-print(dynamicArray(2,queries))
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+
+q = int(first_multiple_input[1])
+
+queries = []
+
+for _ in range(q):
+    queries.append(list(map(int, input().rstrip().split())))
+print(queries)
+print(dynamicArray(n,queries))
 
 
 
