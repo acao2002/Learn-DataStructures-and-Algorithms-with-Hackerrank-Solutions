50e70ce0cc07e2c763e525f4f73ee7d44d3c59ce
acao2002 <xuanan12072002@gmail.com>
2021-02-15 08:34:02 -0500

    add c++

---
+++ DataStructuresC++/Intro.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+private:
+    node *head,*tail;
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+};
+
+int main()
+{
+    linked_list a;
+    return 0;
+}
