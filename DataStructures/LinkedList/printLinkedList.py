ba6df24a9d320c1f86101bbf44a8195f1c4d4ce1
acao2002 <xuanan12072002@gmail.com>
2021-02-16 22:06:26 -0500

    py printlinkedlist

---
+++ DataStructures/LinkedList/printLinkedList.py
@@ -0,0 +1,14 @@
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    while (head!= None):
+        print (head.data)
+        head = head.next
