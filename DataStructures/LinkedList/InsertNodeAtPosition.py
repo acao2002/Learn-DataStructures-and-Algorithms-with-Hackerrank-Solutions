6000d6bc66aa17e1a3d526e39fc264c9d8ad157b
acao2002 <xuanan12072002@gmail.com>
2021-02-17 03:47:08 -0500

    insert node at position py

---
+++ DataStructures/LinkedList/InsertNodeAtPosition.py
@@ -0,0 +1,45 @@
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    p = head
+    for i in range(0,position-1):
+        p = p.next 
+    tmp = SinglyLinkedListNode(data)
+    tmp.next= p.next
+    p.next = tmp  
+    return head 
+
+   
