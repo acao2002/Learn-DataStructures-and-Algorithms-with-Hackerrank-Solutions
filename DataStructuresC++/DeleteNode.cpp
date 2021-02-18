5f30491d9bf033249cf6b55c89c1752df74a16dc
acao2002 <xuanan12072002@gmail.com>
2021-02-18 00:57:08 -0500

    delete node c++

---
+++ DataStructuresC++/DeleteNode.cpp
@@ -0,0 +1,64 @@
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* head, int position) {
+    SinglyLinkedListNode* p = head;
+    if (position == 0) {
+        head = head->next;
+    }
+    else{
+        for(int i =0; i< position -1;i++){
+            p = p->next;
+        }
+        p->next = p->next->next;
+    }
+    return head ;
+
+}
