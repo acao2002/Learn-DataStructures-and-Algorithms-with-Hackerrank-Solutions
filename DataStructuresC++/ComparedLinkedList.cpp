0ac464770585c3d3d6ff6891cb891e1aa28ca621
acao2002 <xuanan12072002@gmail.com>
2021-02-19 03:51:34 -0500

    compare linked list c++

---
+++ DataStructuresC++/ComparedLinkedList.cpp
@@ -0,0 +1,77 @@
+#include <stddef.h>
+#include <iostream>
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the compare_lists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    int result;
+    if (head1 == NULL || head2 == NULL){
+        if (head1 == NULL && head2 == NULL) {
+            result = 1;
+        }
+        else{
+            result = 0;
+        }
+    }
+    else if (head1->data == head2 ->data){
+        result = compare_lists(head1->next,head2->next);
+    }
+    else {
+        result = 0;
+    }
+    return result;
+
+}
