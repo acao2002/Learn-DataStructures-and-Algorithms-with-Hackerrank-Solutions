52497a80e8b85656e60c8392ddce18e9cdca04a7
acao2002 <xuanan12072002@gmail.com>
2021-02-19 02:20:48 -0500

    reverse linked list c++

---
+++ DataStructuresC++/ReverseLL.cpp
@@ -0,0 +1,71 @@
+#include <stddef.h>
+#include <iostream>
+
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* head) {
+if (head == NULL || head->next == NULL) {
+    return head;
+}
+else {
+    SinglyLinkedListNode *remaining = reverse(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return remaining;
+}
+
+}
