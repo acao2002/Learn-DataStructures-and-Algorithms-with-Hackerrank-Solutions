ac410f777cb52fb23b194d937742f407f4ec1119
acao2002 <xuanan12072002@gmail.com>
2021-02-19 02:09:17 -0500

    reverse linked list python

---
+++ DataStructures/ReverseLL.py
@@ -0,0 +1,19 @@
+# Complete the reverse function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reverse(head):
+    
+    if head is None or head.next is None:
+        return head 
+    else:
+        remaining = reverse(head.next)
+        head.next.next = head
+        head.next = None
+        return remaining
