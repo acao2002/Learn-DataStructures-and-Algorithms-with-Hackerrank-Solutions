58df668d10c720b9ae47f6eae057dccc827be8c0
acao2002 <xuanan12072002@gmail.com>
2021-02-21 08:50:52 -0500

    merge ll c++

--- DataStructuresC++/MergeSortedLL.cpp
+++ DataStructuresC++/MergeSortedLL.cpp
@@ -37,17 +37,6 @@ class SinglyLinkedList {
         }
 };
 
-void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
-    while (node) {
-        fout << node->data;
-
-        node = node->next;
-
-        if (node) {
-            fout << sep;
-        }
-    }
-}
 
 void free_singly_linked_list(SinglyLinkedListNode* node) {
     while (node) {
