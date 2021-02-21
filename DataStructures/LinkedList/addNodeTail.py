38bd0fc5739442f672fed7d7a716b18948f923e5
acao2002 <xuanan12072002@gmail.com>
2021-02-21 08:56:02 -0500

    add node tail python

---
+++ DataStructures/LinkedList/addNodeTail.py
@@ -0,0 +1,46 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    tmp = SinglyLinkedListNode(data)
+    p = head
+    if p is None:
+        return tmp
+    else:
+        while p.next is not None:
+            p = p.next
+        p.next = tmp
+        return head
