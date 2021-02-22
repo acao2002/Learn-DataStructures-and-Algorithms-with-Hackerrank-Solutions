66a211d7e1bd58d5159c1affdc4061bb556f78b4
acao2002 <xuanan12072002@gmail.com>
2021-02-21 21:37:50 -0500

    get node value from tail c++

---
+++ DataStructuresC++/LinkedList/getNodeValuefromTail.cpp
@@ -0,0 +1,75 @@
+#include <stddef.h>
+#include <iostream>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the getNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+int getNode(SinglyLinkedListNode* head, int positionFromTail) {
+    int count = 0;
+    SinglyLinkedListNode *current = head;
+    SinglyLinkedListNode *result = head; 
+    
+    while (current != NULL) {
+        current = current ->next;
+        if(count++ > positionFromTail){
+            result = result ->next;
+        }
+        
+    }
+    return result->data;
+
+}
