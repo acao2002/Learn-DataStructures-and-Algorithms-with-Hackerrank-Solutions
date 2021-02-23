1b0a74f8168d612ed6a4f708ffdc53ba30e2e3b2
acao2002 <xuanan12072002@gmail.com>
2021-02-23 10:04:00 -0500

    reverse doubly linked list c++

---
+++ DataStructuresC++/LinkedList/ReverseDoublyLL.cpp
@@ -0,0 +1,77 @@
+#include <stddef.h>
+#include <iostream>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* reverse(DoublyLinkedListNode* head) {
+if (head == NULL || head->next == NULL) {
+    return head;
+}
+else {
+    DoublyLinkedListNode *remaining = reverse(head->next);
+    head->next->prev = head->next->next;
+    head->next->next = head;
+    head->next = NULL;
+    return remaining;
+}
+
+}
