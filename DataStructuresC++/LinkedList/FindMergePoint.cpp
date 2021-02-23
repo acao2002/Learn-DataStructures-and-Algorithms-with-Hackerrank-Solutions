0d83ad4d6cb8cb1231befb80427949ac81194f89
acao2002 <xuanan12072002@gmail.com>
2021-02-23 02:11:48 -0500

    find merge point c++

---
+++ DataStructuresC++/LinkedList/FindMergePoint.cpp
@@ -0,0 +1,89 @@
+#include <stddef.h>
+#include <iostream>
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the findMergeNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *p = head1;
+    SinglyLinkedListNode *p2 = head2;
+    while (p!= p2){
+        if (p->next == NULL){
+            p = head2;
+        }
+        else {
+            p = p->next;
+        }
+        if (p2->next == NULL){
+            p2 = head1;
+        }
+        else {
+            p2 = p2->next;
+        }
+    }
+    return p->data;
+}
