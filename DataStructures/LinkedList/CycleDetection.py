f51f2ebf4b79a83be7266c5a0e3f23bf0b46a313
acao2002 <xuanan12072002@gmail.com>
2021-02-22 22:26:04 -0500

    cycle detection python

--- DataStructures/LinkedList/CycleDetection.py
+++ DataStructures/LinkedList/CycleDetection.py
@@ -21,3 +21,25 @@ def has_cycle(head):
     
     return 0
     
+# Complete the has_cycle function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def has_cycle(head):
+    fast = head
+    slow = head 
+    
+    while (fast is not None and slow is not None):
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:
+            return 1
+    
+    return 0
+    
