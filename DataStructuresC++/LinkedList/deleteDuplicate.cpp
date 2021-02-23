afee6311bee69e3e074ab70eb4214cb7b787c3c9
acao2002 <xuanan12072002@gmail.com>
2021-02-22 21:39:29 -0500

    delete duplicate c++

---
+++ DataStructuresC++/LinkedList/deleteDuplicate.cpp
@@ -0,0 +1,75 @@
+#include <stddef.h>
+#include <iostream>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode *p = head;
+    SinglyLinkedListNode *current = head; 
+    if (p->next == NULL){
+        return current;
+    }
+    if (p->next->data > p->data && p->next != NULL){
+        current->next = removeDuplicates(p->next);
+        return current;
+    }
+    else {
+        return removeDuplicates(p->next);
+    }
+
+}
