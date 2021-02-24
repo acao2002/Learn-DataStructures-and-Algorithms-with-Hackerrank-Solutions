60fdf5414688e3971ce9e56c4499ff1fc59cee65
acao2002 <xuanan12072002@gmail.com>
2021-02-24 08:43:40 -0500

    addNodeSorted Doubly LL python

---
+++ DataStructures/LinkedList/InsertNodedDoublyLinkedList.py
@@ -0,0 +1,68 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the sortedInsert function below.
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+def sortedInsert(head, data):
+    temp = DoublyLinkedListNode(data)
+    if head is None:
+        return temp
+    if temp.data >= head.data:
+        head.next = sortedInsert(head.next,data)
+        return head
+    else:
+        if head.prev is None:
+            head.prev = temp
+            temp.next = head
+        else:
+            head.prev.next = temp
+            temp.prev = head.prev
+            head.prev = temp
+            temp.next = head
+        return temp
+        
