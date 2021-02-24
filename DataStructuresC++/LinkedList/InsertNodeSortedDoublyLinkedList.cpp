b3114006a40f2b5cdced38d0bcb7d8d1f57e6f0e
acao2002 <xuanan12072002@gmail.com>
2021-02-24 09:05:16 -0500

    addNodeSorted Doubly LL c++

---
+++ DataStructuresC++/LinkedList/InsertNodeSortedDoublyLinkedList.cpp
@@ -0,0 +1,90 @@
+#include <stddef.h>
+#include <iostream>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    
+    DoublyLinkedListNode* temp = new DoublyLinkedListNode(data);
+    if (head == NULL){
+        return temp;
+    }
+    if (temp->data >= head->data) {
+        head->next = sortedInsert(head->next,data);
+        return head;
+    }
+    else {
+        if (head->prev == NULL) {
+            head->prev = temp;
+            temp->next = head;
+        }
+        else{
+            head->prev->next = temp;
+            temp->prev = head->prev;
+            head->prev = temp;
+            temp->next = head;
+            }
+        return temp;
+    }
+    
+
+}
