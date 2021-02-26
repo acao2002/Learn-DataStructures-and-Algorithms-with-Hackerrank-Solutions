9a18326213ee19225a8c09ecc445f3b446bd26e1
acao2002 <xuanan12072002@gmail.com>
2021-02-25 21:59:01 -0500

    preOrder Traversal tree c++

---
+++ DataStructuresC++/Tree/PreorderTraversal.cpp
@@ -0,0 +1,62 @@
+#include <stddef.h>
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void preOrder(Node *root) {
+        if (root != NULL){
+            cout << root->data;
+            cout << " ";
+            preOrder(root->left);
+            preOrder(root->right);
+        }
+
+    }};
