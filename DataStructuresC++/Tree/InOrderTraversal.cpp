ff9169d85159b0d1326de37fa80baa04393a18ea
acao2002 <xuanan12072002@gmail.com>
2021-02-25 22:50:59 -0500

    InOrderTraversal c++

---
+++ DataStructuresC++/Tree/InOrderTraversal.cpp
@@ -0,0 +1,66 @@
+#include <stddef.h>
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void inOrder(Node *root) {
+        if (root != NULL) {
+            if (root->left){
+                inOrder(root->left);
+            }
+                
+            cout << root->data;
+            cout << " ";
+            inOrder(root->right);
+        }
+      
+    }
+
+};
