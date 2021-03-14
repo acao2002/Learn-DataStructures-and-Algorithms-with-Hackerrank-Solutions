ec121d18dfb25143d30a4d0a01dccbbde70c1ca5
acao2002 <xuanan12072002@gmail.com>
2021-03-14 03:42:59 -0400

    modified Fibo

---
+++ Algorithms Problems/Dynamic Programming/modifiedFibonacci.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the fibonacciModified function below.
+def fibonacciModified(t1, t2, n):
+    f = [0]*(n+1)
+    f[0] = t1
+    f[1] = t2
+    for i in range(2,n+1):
+        f[i] = f[i-2]+f[i-1]*f[i-1]
+    return f[n-1]
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t1T2n = input().split()
+
+    t1 = int(t1T2n[0])
+
+    t2 = int(t1T2n[1])
+
+    n = int(t1T2n[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
