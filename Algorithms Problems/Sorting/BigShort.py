e00e3a67093f046250e813160b1e4ee691a0b60e
acao2002 <xuanan12072002@gmail.com>
2021-03-15 05:04:18 -0400

    big short optimized

--- Algorithms Problems/Sorting/BigShort.py
+++ Algorithms Problems/Sorting/BigShort.py
@@ -6,9 +6,9 @@ import random
 import re
 import sys
 
-# Complete the bigSorting function below.
+# dont convert from string to int because it will hinder performance
 def bigSorting(unsorted):
-    for s in sorted(unsorted, key=int):
+    for s in sorted(unsorted, key = lambda x: (len(x),x)):
         print(s)
 
    
